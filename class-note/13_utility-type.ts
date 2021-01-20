bd7746a4d3455d7dac91123af7abacce4a48f0db
harrykim14 <harrykim14@korea.ac.kr>
2021-01-21 02:13:58 +0900

    21. 01. 21
    ts class chapter 06: summary of implement utilty type

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -7,7 +7,7 @@ interface Product {
 }
 
 function fetchProducts(): Promise<Product[]> {
-
+    
 }
 
 // interface ProductDetail {
@@ -25,5 +25,51 @@ function displayProductDetail(shoppingItem: ShoppingItem) {
 // Omit<'이미 정의된 타입', '해당 타입 내에서 제거할 필드명(마찬가지로 여러개일 경우 |로 구분)'>
 type ShoppingItem = Omit<Product, 'brand'|'stock'>
 function displayProductDetail(shoppingItem: ShoppingItem) {
+
+}
+
+interface UpdateProduct {
+    id?: number;
+    name?: string;
+    price?: number;
+    brand?: string;
+    stock?: number;
+}
+
+/* Partial의 상세코드
+    type Required<T> = {
+        [P in keyof T]-?: T[P];
+    };
+*/
+
+// 풀어써보면 이렇게 된다
+type UpdateProductEach {
+    id?: Product['id'];
+    name?: Product['name'];
+    price?: Product['price'];
+    brand?: Product['brand'];
+    stock?: Product['stock'];
+}
+
+// 조금 짧게 줄여보자
+type UpdateProductShorten {
+    [p in 'id' | 'name' | 'price' | 'brand' | 'stock' ]?: Product[p]
+}
+
+// 더 짧게 줄여서 상세코드와 비슷해진다
+type UpdateProductKeys = keyof Product;
+type UpdateProductPartial {
+    [p in keyof Product]?: Product[p];
+    // 첫번째 루프에서  id?: Product['id'];를, 두번째 루프에서 name?: Product['name']; ... 처럼 적용된다
+}
+
+// <T>와 같이 타입 제네릭을 사용하면 Product에 국한되지 않는 코드로 변경된다
+type Subset<T> = {
+    [p in keyof T]?: T[p];
+}
+
+
+// UpdateProduct type과 같은 효과를 낸다
+function updateProductItem(productItem: Partial<Product>) {
 
 }
